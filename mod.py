00fdc70754d81dbe7ae3f2ab342b9d2b1678d8e0
Aruchok <39999420+Aruchok@users.noreply.github.com>
2020-10-22 17:56:25 +0400

    Fix commit

--- mod.py
+++ mod.py
@@ -4,31 +4,26 @@ from sqlalchemy import create_engine, Table, Column, String, Float, MetaData
 from sqlalchemy.sql import select
 response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Volgograd&appid=3a25edb8b1d5f13a36e4cbd2af97e515")
 
-weather_json = response.json()
+class WeatherProvider:
+    def __init__(self, key):
+        self.key = key
 
-Temp = weather_json['main']['temp']
-Feels_like = weather_json['main']['feels_like']
-Temp_min = weather_json['main']['temp_min']
-Temp_max = weather_json['main']['temp_max']
-Wind_speed = weather_json['wind']['speed']
-Humidity = weather_json['main']['humidity']
+    def get_data(self):
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=Volgograd&appid=3a25edb8b1d5f13a36e4cbd2af97e515'
+        data = requests.get(url).json()
+        return [
+            {
+                'Temp':data['main']['temp'],
+                'Feels_like':data['main']['feels_like'],
+                'Temp_min':data['main']['temp_min'],
+                'Temp_max':data['main']['temp_max'],
+                'Humidity':data['main']['humidity'],
+            }
+        ]
 
-result = [(Temp - 273, Feels_like - 273, Temp_min - 273, Temp_max - 273, Wind_speed, Humidity)]
 
-connect = sqlite3.connect("weather_data.db")
-cursor = connect.cursor()
+def db_meth(str_conn, data_name):
 
-<<<<<<< Updated upstream
-cursor.execute("""CREATE TABLE weather
-                  (Temp float, Feels_like float, Temp_min float,
-                   Temp_max float, Wind_speed float, Humidity float)
-               """)
-cursor.executemany("INSERT INTO weather VALUES (?,?,?,?,?,?)", result)
-connect.commit()
-
-print("Температура = ", result[0][0], "Чувствуется = ", result[0][1], "Максимальная = ", result[0][2], 
-      "Минимальная = ", result[0][3], "Скорость ветра = ", result[0][4], "Давление = ", result[0][5])
-=======
     engine = create_engine(str_conn)
     metadata = MetaData()
     weather = Table(
@@ -76,6 +71,3 @@ for row in c.execute(select([weather])):
 
 # print("Температура = ", result[0][0], "Чувствуется = ", result[0][1], "Максимальная = ", result[0][2], 
 #       "Минимальная = ", result[0][3], "Скорость ветра = ", result[0][4], "Давление = ", result[0][5])
->>>>>>> Stashed changes
-
-
